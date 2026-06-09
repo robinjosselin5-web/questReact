1f9267b00ff17273ae14dc475748f713c5d94003
Josselin <robinjosselin5@gmail.com>
2026-06-09 13:55:17 +0200

    Part 5 of React quest WCS - props.2

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,10 +1,17 @@
+// On déclare le "Typage" -> interface/structure des données des props
+// Pour voir si cela fonction -> changer 'name: string' par 'name: number' -> une erreur apparait dans la fonction
+interface PokemonCardProps {
+  name: string,
+  imgSrc?: string
+}
 
-function PokemonCard({pokemon}) {
-  
+// On passe dans la fonction l'interface "Le format des données/Typages" 
+function PokemonCard( {name, imgSrc}: PokemonCardProps) {
+
   return (
     <figure>
-      <img src={pokemon.imgSrc} alt={pokemon.name}></img>
-      <figcaption>{pokemon.name}</figcaption>
+      <img src={imgSrc} alt={name}></img>
+      <figcaption>{name}</figcaption>
     </figure>
   );
 
