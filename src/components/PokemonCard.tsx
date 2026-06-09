a1b834b4cecb2b517090b6ab96ad00977a2561a3
Josselin <robinjosselin5@gmail.com>
2026-06-09 09:40:33 +0200

    Step 3 quest WCS

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,10 +1,24 @@
-function PokemonCard() {
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
-  
-      return <figure>
-                 <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Pokemon Bulbasaur"></img>
-                 <figcaption>Bulbasaur</figcaption>
-             </figure>;
+function PokemonCard() {
+    
+    const pokemon = pokemonList[2];
+
+    return <figure>
+        {   pokemon.imgSrc ?
+                (<img src={pokemon.imgSrc} alt={pokemon.name}></img>) : (<p>???</p>)
+        }
+                <figcaption>{pokemon.name}</figcaption>
+            </figure>;
 
 }
 
