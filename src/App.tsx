03fafaf920704ebcce4bf357bf7fbc206f8646ed
Josselin <robinjosselin5@gmail.com>
2026-06-09 11:20:15 +0200

    Step 4 - Props 1 WCS quest React

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import "./App.css";
 
-import MyTitle from "./components/MyTitle";
 import PokemonCard from "./components/PokemonCard";
+import MyTitle from "./components/MyTitle";
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 function App() {
   return (
     <section>
       <MyTitle />
-      <PokemonCard />
+      <PokemonCard pokemon={pokemonList[0]}/>
     </section>
   );
 }
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,24 +1,12 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
 
-function PokemonCard() {
-    
-    const pokemon = pokemonList[2];
-
-    return <figure>
-        {   pokemon.imgSrc ?
-                (<img src={pokemon.imgSrc} alt={pokemon.name}></img>) : (<p>???</p>)
-        }
-                <figcaption>{pokemon.name}</figcaption>
-            </figure>;
+function PokemonCard({pokemon}) {
+  
+  return (
+    <figure>
+      <img src={pokemon.imgSrc} alt={pokemon.name}></img>
+      <figcaption>{pokemon.name}</figcaption>
+    </figure>
+  );
 
 }
 
